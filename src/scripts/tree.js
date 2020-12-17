9e66019836994774cb1ce13866008aec661a105e
ctcuff <dev.ctcuff@gmail.com>
2020-12-17 16:33:28 -0500

    Fixed crash that occurred when searching for some repositories
    This occurred because the GitHub API returns 3 types: 'blobs',
    'trees', and 'commits'. Since 'blobs' and 'trees' are displayed as
    files and folders, the FileExplorer component didn't know how to display
    nodes with the type 'commit'. For now, nodes with type 'commit' are ignored.
    Maybe this'll be a feature in the future???

--- src/scripts/tree.js
+++ src/scripts/tree.js
@@ -1,13 +1,61 @@
 class Tree {
+  /**
+   * Takes the response from the GitHub tree API and turns it into a flat
+   * object with the path of each folder/file. For example
+   *
+   * ```
+   {
+      "src": {
+        "path": "src",
+        "type": "folder",
+        "children": [
+          "src/App.vue",
+          "src/assets",
+          "src/components",
+          "src/main.js",
+          "src/project-info.js",
+          "src/router.js",
+          "src/scss",
+          "src/util"
+        ],
+        "url": "https://api.github.com/repos/ctcuff/ctcuff.github.io/git/trees/358553e5a2ee066d0bb0f52e4033456921a83c3f",
+        "isRoot": true
+      },
+      "src/App.vue": {
+        "type": "file",
+        "name": "App.vue",
+        "path": "src/App.vue",
+        "url": "https://api.github.com/repos/ctcuff/ctcuff.github.io/git/blobs/2e6719d25b30a897967c849672ccee435f5c6ae9"
+      },
+      .
+      .
+      .
+    }
+   * ```
+   *
+   * @see https://docs.github.com/en/free-pro-team@latest/rest/reference/git#trees
+   */
   static treeify(data) {
     const tree = {}
 
-    data.forEach(item => {
-      const { path, url } = item
+    for (const item of data) {
+      const { path, url, type } = item
+
+      if (type !== 'blob' && type !== 'tree') {
+        // The GitHub API has 3 types: blobs, trees, and commits.
+        // Blobs and trees are equivalent to files and folders so
+        // we only care about adding those types since we
+        // can't display commits (yet?).
+        continue
+      }
+
+      // If this file/folder is contained in a folder, split the path
+      // to try and find the name of the parent folder. Frr example:
+      // `src/components/App.js` => `components`
       const parts = path.split('/')
       const parent = parts.splice(0, parts.length - 1).join('/')
 
-      switch (item.type) {
+      switch (type) {
         case 'blob':
           tree[path] = {
             type: 'file',
@@ -26,16 +74,16 @@ class Tree {
             }
           }
           break
-        default:
-          console.warn('Invalid type', item.type)
       }
 
+      // If the file/folder had a parent, make sure that file/folder
+      // gets added to the parent's children
       if (parent) {
         tree[parent].children.push(path)
       } else {
         tree[path].isRoot = true
       }
-    })
+    }
 
     return tree
   }
