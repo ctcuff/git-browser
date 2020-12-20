df83a77b4277e84513f648d73c22aaaadd4f3e92
ctcuff <dev.ctcuff@gmail.com>
2020-12-19 23:56:42 -0500

    An overlay is added when resizing
    To prevent the cursor flicker that occurs when moving the resize
    panel, an overlay is added over the app. This also prevents the
    cursor from activating hover states of other elements when resizing.

--- src/components/App.js
+++ src/components/App.js
@@ -182,6 +182,7 @@ class App extends React.Component {
 
     return (
       <div className={`app ${colorClass}`}>
+        <div className="resize-overlay" />
         <ExplorerPanel
           onSelectFile={this.onSelectFile}
           onSearchFinished={this.closeAllTabs}
