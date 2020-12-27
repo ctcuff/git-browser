c82b5fd7c0693dc5b09d86df22ace7361e1072fc
ctcuff <dev.ctcuff@gmail.com>
2020-12-27 18:32:33 -0500

    Separated resize panel end explorer panel components

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -101,7 +101,7 @@ class Editor extends React.Component {
   render() {
     return (
       <div className="monaco-editor-container" ref={this.editorRef}>
-        {this.state.isLoading && <LoadingOverlay text="Loading editor..." />}
+        {this.state.isLoading && <LoadingOverlay text="Loading viewer..." />}
       </div>
     )
   }
